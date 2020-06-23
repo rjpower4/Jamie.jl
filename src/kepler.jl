9de522b8f9e720ba5f570c83488945d3f6285761
Rolfe Power <rolfepower4@gmail.com>
2020-06-23 14:00:02 -0500

    Add gm, pv -> KeplerianElementSet

--- src/kepler.jl
+++ src/kepler.jl
@@ -172,3 +172,83 @@ define by the `AbstractBody`.
 """
 angular_momentum(ab::AbstractBody, ke::KeplerianElementSet) =
     angular_momentum(gravitational_parameter(ab), ke)
+
+"""
+    KeplerianElementSet(gm, pv::PositionVelocity)
+
+Convert position-velocity values to Keplerian elements given the gravitational parameter.
+The position and velocity components are assumed to be defined in an inertial frame with its 
+basepoint at the primary with parameter, `gm`. 
+The resultant Keplerian element set will be, therefore, defined with respect to that same frame.
+"""
+function KeplerianElementSet(gm, pv::PositionVelocity)
+    R = pv_position(pv)
+    V = pv_velocity(pv)
+    r = norm(R)
+    v = norm(V)
+
+    spec_energy = 0.5 * v^2 - gm / r
+    a = -gm ./ (2spec_energy)
+
+    H = cross(R, V)
+    h = norm(H)
+
+    i = acos(H[3] / h)
+
+    eK = @SVector [0.0, 0.0, 1.0]
+    nodal_axis = cross(eK, H) ./ h
+    nodal_axis_unit = nodal_axis ./ norm(nodal_axis)
+
+    e_vec = (1.0 / gm) .* cross(V, H) .- R ./ r
+    e = norm(e_vec)
+
+    if H[2] == 0.0
+        nodal_axis_unit = @SVector [cos(0.0), sin(0.0), 0.0]
+    end
+    Ω = acos(nodal_axis_unit[1])
+    if nodal_axis_unit[2] < 0.0
+        Ω = 2π - Ω
+    end
+
+    ω = acos(dot(nodal_axis_unit, e_vec) ./ e ./ norm(nodal_axis_unit))
+    if i != 0.0 && e_vec[3] < 0.0
+        ω = 2π - ω
+    end
+    if i == 0.0 && e_vec[2] < 0.0
+        ω = -ω
+    end
+
+    θ = 0.0
+    if dot(V, R) > 0
+        θ = acos(dot(e_vec, R) ./ (r * e))
+    else
+        θ = 2π - acos(dot(e_vec, R) ./ (r * e))
+    end
+
+    KeplerianElementSet(
+        a,
+        e,
+        i,
+        ω,
+        Ω,
+        θ
+    )
+end
+
+"""
+    KeplerianElementSet(::AbstractBody, ::PositionVelocity)
+
+Convert position-velocity values to Keplerian elements given the defined body.
+The body is assumed to be centrobaric, i.e., the gm value is simply queried then used directly.
+The position and velocity components are assumed to be defined in an inertial frame with its 
+basepoint at the center of the body.
+The resultant Keplerian element set will be, therefore, defined with respect to that same frame
+and basepoint.
+
+See also:
+[`CelestialBody`](@ref),
+[`gravitational_parameter`](@ref),
+"""
+function KeplerianElementSet(ab::AbstractBody, pv::PositionVelocity)
+    KeplerianElementSet(gravitational_parameter(ab), pv)
+end
