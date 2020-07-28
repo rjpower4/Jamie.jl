58e9d7b57f0aab4dd78d222ac014b9570c680e37
Rolfe Power <rolfepower4@gmail.com>
2020-07-27 23:14:30 -0400

    Update body tests to reflect refactoring

--- test/body.jl
+++ test/body.jl
@@ -1,19 +1,8 @@
 @testset "Bodies" begin
     earth_name = "EARTH"
     earth_spice_id = 399
-    earth_potential = PointMassPotential(398600.4354360959)
-    earth_shape = SphericalShapeModel(6371.008366666666)
-    earth = CelestialBody(
-        earth_name, 
-        earth_spice_id, 
-        earth_potential, 
-        earth_shape
-    ) 
+    earth = Jamie.Earth()
 
     @test name_string(earth) == earth_name
     @test spice_identifier(earth) == earth_spice_id
-    @test potential_model(earth) == earth_potential
-    @test gravitational_parameter(earth) == gravitational_parameter(earth_potential)
-    @test mean_radius(earth) == mean_radius(earth_shape)
-    @test shape_model(earth) == earth_shape
 end
