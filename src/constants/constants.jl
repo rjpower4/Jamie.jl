d608d09e7e1f7ad854d2cbaf8fd746e00aa45b84
Rolfe Power <rolfepower4@gmail.com>
2020-07-29 16:49:39 -0500

    Add documentation

--- src/constants/constants.jl
+++ src/constants/constants.jl
@@ -1,4 +1,16 @@
+"""
+    GRAVITATIONAL_CONSTANT
+
+The standard Newtonian gravitational constant in km^3 / (kg s^2).
+The value is 6.674e-20.
+"""
 const GRAVITATIONAL_CONSTANT = 6.6743e-20 # km^3 / (kg s^2)
+
+"""
+    SPEED_OF_LIGHT
+
+The speed of light in a vacuum in km/s as defined by NIST.
+"""
 const SPEED_OF_LIGHT = 299792.458 # km / s
 
 include("integration.jl")
